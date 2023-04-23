18b8a584ab1dc5c65266576e4583c5bb588187eb
Jhonatan Neves <nevesjn2@gmail.com>
2023-04-23 11:37:12 -0300

    imc activity

--- app/src/main/java/com/example/appfitness/ImcActivity.kt
+++ app/src/main/java/com/example/appfitness/ImcActivity.kt
@@ -2,10 +2,36 @@ package com.example.appfitness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener{
+
+        }
     }
+
+    private fun validate(): Boolean {
+        if (editWeight.text.toString().isNotEmpty()
+            && editHeight.text.toString().isNotEmpty()
+            && !editWeight.text.toString().startsWith("0")
+            && !editHeight.text.toString().startsWith("0")) {
+            return true
+        } else {
+            return false
+        }
+    }
+
 }
