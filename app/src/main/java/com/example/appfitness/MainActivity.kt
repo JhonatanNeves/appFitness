e4f495fdd82999778df385396998959a8406328e
Jhonatan Neves <nevesjn2@gmail.com>
2023-05-04 19:26:48 -0300

    dynamic data

--- app/src/main/java/com/example/appfitness/MainActivity.kt
+++ app/src/main/java/com/example/appfitness/MainActivity.kt
@@ -1,24 +1,44 @@
 package com.example.appfitness
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var btnImc: LinearLayout
+    //    private lateinit var btnImc: LinearLayout
     private lateinit var rvMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = MainAdapter()
+        val mainItems = mutableListOf<MainItem>()
+        mainItems.add(
+            MainItem(
+                id = 1,
+                drawId = R.drawable.icon_btn,
+                texStringId = R.string.imc,
+                color = Color.GRAY
+            )
+        )
+        mainItems.add(
+            MainItem(
+                id = 2,
+                drawId = R.drawable.icon_btn,
+                texStringId = R.string.tmb,
+                color = Color.GRAY
+            )
+        )
+
+        val adapter = MainAdapter(mainItems)
         rvMain = findViewById(R.id.rv_main)
         rvMain.adapter = adapter
         rvMain.layoutManager = LinearLayoutManager(this)
@@ -31,7 +51,8 @@ class MainActivity : AppCompatActivity() {
 //        }
     }
 
-    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
+    private inner class MainAdapter(private val mainItems: List<MainItem>) :
+        RecyclerView.Adapter<MainViewHolder>() {
 
         // layout XML (item)
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
@@ -41,16 +62,21 @@ class MainActivity : AppCompatActivity() {
 
         // disparado toda vez que houver uma rolagem na tela e for necessario trocar de conteudo da celula
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            val itemCurrent = mainItems[position]
+            holder.bind(itemCurrent)
         }
 
         override fun getItemCount(): Int {
-            return 30
+            return mainItems.size
         }
 
     }
 
-    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view){
-
+    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: MainItem) {
+            val buttonTest: Button = itemView.findViewById(R.id.btn_item)
+            buttonTest.setText(item.texStringId)
+        }
     }
 
 }
