d1a9d8d2a561a90b6bc9ebc0c5ef2bf65c98eb96
Jhonatan Neves <nevesjn2@gmail.com>
2023-05-10 18:56:29 -0300

    data-base

--- app/src/main/java/com/example/appfitness/model/AppDatabase.kt
+++ app/src/main/java/com/example/appfitness/model/AppDatabase.kt
@@ -1,7 +1,32 @@
 package com.example.appfitness.model
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [Calc::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun calcDao(): CalcDao
+
+    companion object{
+
+        private var INSTANCE: AppDatabase?= null
+
+        fun getDatabase(context: Context) : AppDatabase{
+            if (INSTANCE == null) {
+
+                synchronized(this){
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "fitness_tracker"
+                    ).build()
+                }
+
+            }else {
+                return INSTANCE
+            }
+        }
+    }
 }
