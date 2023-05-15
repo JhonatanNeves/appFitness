e6869a2c5ece237dd326c2cfbef5a0bbb93402a3
Jhonatan Neves <nevesjn2@gmail.com>
2023-05-15 19:55:34 -0300

    result-list-data

--- app/src/main/java/com/example/appfitness/ListCalcActivity.kt
+++ app/src/main/java/com/example/appfitness/ListCalcActivity.kt
@@ -3,14 +3,29 @@ package com.example.appfitness
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.appfitness.model.Calc
 
 class ListCalcActivity : AppCompatActivity() {
+
+    private lateinit var rv: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_calc)
 
-        val type = intent?.extras?.getString("type") ?: throw IllegalStateException("type not found")
+        rv = findViewById(R.id.rv_list)
+        rv.layoutManager = LinearLayoutManager(this)
+
+
+        val type =
+            intent?.extras?.getString("type") ?: throw IllegalStateException("type not found")
         Thread {
             val app = (application as App)
             val dao = app.db.calcDao()
@@ -20,5 +35,34 @@ class ListCalcActivity : AppCompatActivity() {
 
             }
         }.start()
+    }
+
+    private inner class ListCalcAdapter(
+        private val listCalc: List<Calc>,
+//        private val onItemClickListener: OnItemClickListener
+    ) : RecyclerView.Adapter<ListCalcAdapter.ListCalcViewHolder>() {
+
+        // layout XML (item)
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListCalcViewHolder {
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+            return ListCalcViewHolder(view)
+        }
+
+        // disparado toda vez que houver uma rolagem na tela e for necessario trocar de conteudo da celula
+        override fun onBindViewHolder(holder: ListCalcViewHolder, position: Int) {
+            val itemCurrent = listCalc[position]
+            holder.bind(itemCurrent)
+        }
+
+        override fun getItemCount(): Int {
+            return listCalc.size
+        }
+
+        private inner class ListCalcViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(item: Calc) {
+
+                }
+            }
+        }
     }
 }
