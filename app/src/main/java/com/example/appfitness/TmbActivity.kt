c3babf3f1cdb9d7e293ebdc932c310f31de1bbd5
Jhonatan Neves <nevesjn2@gmail.com>
2023-05-18 20:17:26 -0300

    Auto-Complete-Text

--- app/src/main/java/com/example/appfitness/TmbActivity.kt
+++ app/src/main/java/com/example/appfitness/TmbActivity.kt
@@ -1,17 +1,26 @@
 package com.example.appfitness
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.*
 
 class TmbActivity : AppCompatActivity() {
 
     private lateinit var lifestyle: AutoCompleteTextView
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+    private lateinit var editAge: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tmb)
+
+        editWeight = findViewById(R.id.edit_tmb_weight)
+        editHeight = findViewById(R.id.edit_tmb_height)
+        editAge = findViewById(R.id.edit_tmb_age)
 
         lifestyle = findViewById(R.id.auto_lifestyle)
         val items = resources.getStringArray(R.array.tbm_lifestyle)
@@ -19,5 +28,41 @@ class TmbActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
         lifestyle.setAdapter(adapter)
 
+        val btnSend: Button = findViewById(R.id.btn_tmb_send)
+        btnSend.setOnClickListener{
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_search) {
+            finish()
+            openListActivity()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openListActivity() {
+        val intent = Intent(this, ListCalcActivity::class.java)
+        intent.putExtra("type", "imc")
+        startActivity(intent)
+    }
+
+    private fun validate(): Boolean {
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && editAge.text.toString().isNotEmpty()
+                && !editWeight.text.toString().startsWith("0")
+                && !editHeight.text.toString().startsWith("0")
+                && !editAge.text.toString().startsWith("0"))
+    }
+
 }
